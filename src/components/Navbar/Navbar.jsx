7f7d9831ba1ca4d289aaeef2ce7abc57f5e342e4
OGENDI <demokonge17@gmail.com>
2023-03-21 23:09:47 +0300

    Styling the navbar on small screens

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -20,12 +20,11 @@ const Navbar = () => {
         <li className='p__opensans'><a href='#menu'>Menu</a></li>
         <li className='p__opensans'><a href='#awards'>Awards</a></li>
         <li className='p__opensans'><a href='#contact'>Contact</a></li>
-
       </ul>
+      
       <div className='app__navbar-login'>
         <a href='#login' className='p__opensans'>Log In / Register </a>
-
-        <div />
+        <div/>
         <a href='/' className='p__opensans'>Book Table</a>
       </div>
       <div className='app__navbar-smallscreen'>
@@ -33,8 +32,7 @@ const Navbar = () => {
         
         {toggleMenu && (
         <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
-          <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={() => setToggleMenu(false)} />
-        
+          <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={() => setToggleMenu(false)} />        
           <ul className='app__navbar-smallscreen-links'>
             <li className='p__opensans'><a href='#home'>Home</a></li>
             <li className='p__opensans'><a href='#about'>About</a></li>
