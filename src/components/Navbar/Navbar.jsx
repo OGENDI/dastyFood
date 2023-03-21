5a44b77c7b8b7e00e41b748da0fb3f536fa6d079
OGENDI <demokonge17@gmail.com>
2023-03-21 23:36:42 +0300

    Add more styles on overlay

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
         <li className='p__opensans'><a href='#awards'>Awards</a></li>
         <li className='p__opensans'><a href='#contact'>Contact</a></li>
       </ul>
-      
+
       <div className='app__navbar-login'>
         <a href='#login' className='p__opensans'>Log In / Register </a>
         <div/>
@@ -33,7 +33,7 @@ const Navbar = () => {
         {toggleMenu && (
         <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
           <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={() => setToggleMenu(false)} />        
-          <ul className='app__navbar-smallscreen-links'>
+          <ul className='app__navbar-smallscreen_links'>
             <li className='p__opensans'><a href='#home'>Home</a></li>
             <li className='p__opensans'><a href='#about'>About</a></li>
             <li className='p__opensans'><a href='#menu'>Menu</a></li>
